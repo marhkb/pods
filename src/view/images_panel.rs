3112c28118c1648f9fe4897f09421487051ee5f1
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-03-14 17:30:28 +0100

    images-panel: Remove unused id `preferences_page`

--- src/view/images_panel.rs
+++ src/view/images_panel.rs
@@ -30,8 +30,6 @@ mod imp {
         #[template_child]
         pub(super) progress_bar: TemplateChild<gtk::ProgressBar>,
         #[template_child]
-        pub(super) preferences_page: TemplateChild<adw::PreferencesPage>,
-        #[template_child]
         pub(super) image_group: TemplateChild<adw::PreferencesGroup>,
         #[template_child]
         pub(super) list_box: TemplateChild<gtk::ListBox>,
