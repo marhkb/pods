7938395ae11b55acd02e512c2ee4253c1d9f5b48
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-03-14 18:11:26 +0100

    images-panel: Update filter on event

--- src/view/images_panel.rs
+++ src/view/images_panel.rs
@@ -191,6 +191,13 @@ mod imp {
                 }));
             let filter_model = gtk::FilterListModel::new(Some(obj.image_list()), Some(&filter));
 
+            obj.image_list().connect_notify_local(
+                Some("fetched"),
+                clone!(@weak filter => move |_ ,_| {
+                    filter.changed(gtk::FilterChange::Different);
+                }),
+            );
+
             obj.set_list_box_visibility(filter_model.upcast_ref());
             filter_model.connect_items_changed(clone!(@weak obj => move |model, _, _, _| {
                 obj.set_list_box_visibility(model.upcast_ref());
