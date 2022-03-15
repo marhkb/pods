3f2786be716e218231904322aab8f3c61d738da9
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-03-15 20:47:06 +0100

    images-prune-dialog: Update filter on `fetched` event

--- src/view/images_prune_dialog.rs
+++ src/view/images_prune_dialog.rs
@@ -14,6 +14,7 @@ mod imp {
     #[derive(Debug, Default, CompositeTemplate)]
     #[template(resource = "/com/github/marhkb/Symphony/ui/images-prune-dialog.ui")]
     pub(crate) struct ImagesPruneDialog {
+        pub(super) image_list: OnceCell<model::ImageList>,
         pub(super) images_to_prune: OnceCell<gtk::NoSelection>,
         #[template_child]
         pub(super) button_prune: TemplateChild<gtk::Button>,
@@ -41,32 +42,43 @@ mod imp {
     impl ObjectImpl for ImagesPruneDialog {
         fn properties() -> &'static [glib::ParamSpec] {
             static PROPERTIES: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
-                vec![glib::ParamSpecObject::new(
-                    "images-to-prune",
-                    "Images To Prune",
-                    "The images to prune",
-                    gtk::NoSelection::static_type(),
-                    glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
-                )]
+                vec![
+                    glib::ParamSpecObject::new(
+                        "image-list",
+                        "Image List",
+                        "The list of images",
+                        model::ImageList::static_type(),
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                    ),
+                    glib::ParamSpecObject::new(
+                        "images-to-prune",
+                        "Images To Prune",
+                        "The images to prune",
+                        gtk::NoSelection::static_type(),
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::EXPLICIT_NOTIFY,
+                    ),
+                ]
             });
             PROPERTIES.as_ref()
         }
 
         fn set_property(
             &self,
-            _obj: &Self::Type,
+            obj: &Self::Type,
             _id: usize,
             value: &glib::Value,
             pspec: &glib::ParamSpec,
         ) {
             match pspec.name() {
-                "images-to-prune" => self.images_to_prune.set(value.get().unwrap()).unwrap(),
+                "image-list" => self.image_list.set(value.get().unwrap()).unwrap(),
+                "images-to-prune" => obj.set_images_to_prune(value.get().unwrap()),
                 _ => unimplemented!(),
             }
         }
 
         fn property(&self, obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
             match pspec.name() {
+                "image-list" => self.image_list.get().to_value(),
                 "images-to-prune" => obj.images_to_prune().to_value(),
                 _ => unimplemented!(),
             }
@@ -74,6 +86,24 @@ mod imp {
 
         fn constructed(&self, obj: &Self::Type) {
             self.parent_constructed(obj);
+
+            let image_list = self.image_list.get().unwrap();
+
+            let filter =
+                gtk::CustomFilter::new(clone!(@weak obj => @default-return false, move |item| {
+                    let image = item.downcast_ref::<model::Image>().unwrap();
+                    image.dangling() || image.containers() == 0
+                }));
+
+            image_list.connect_notify_local(
+                Some("fetched"),
+                clone!(@weak filter => move |_ ,_| filter.changed(gtk::FilterChange::Different)),
+            );
+
+            obj.set_images_to_prune(gtk::NoSelection::new(Some(&gtk::FilterListModel::new(
+                Some(image_list),
+                Some(&filter),
+            ))));
 
             obj.set_visible();
             obj.images_to_prune()
@@ -94,27 +124,23 @@ glib::wrapper! {
 }
 
 impl From<&model::ImageList> for ImagesPruneDialog {
-    fn from(images: &model::ImageList) -> Self {
-        glib::Object::new(&[
-            (
-                "images-to-prune",
-                &gtk::NoSelection::new(Some(&gtk::FilterListModel::new(
-                    Some(images),
-                    Some(&gtk::CustomFilter::new(|obj| {
-                        let image = obj.downcast_ref::<model::Image>().unwrap();
-                        image.dangling() || image.containers() == 0
-                    })),
-                ))),
-            ),
-            ("use-header-bar", &1),
-        ])
-        .expect("Failed to create ImagesPruneDialog")
+    fn from(image_list: &model::ImageList) -> Self {
+        glib::Object::new(&[("image-list", image_list), ("use-header-bar", &1)])
+            .expect("Failed to create ImagesPruneDialog")
     }
 }
 
 impl ImagesPruneDialog {
     pub(crate) fn images_to_prune(&self) -> Option<&gtk::NoSelection> {
         self.imp().images_to_prune.get()
+    }
+
+    pub(crate) fn set_images_to_prune(&self, value: gtk::NoSelection) {
+        if self.images_to_prune() == Some(&value) {
+            return;
+        }
+        self.imp().images_to_prune.set(value).unwrap();
+        self.notify("images-to-prune");
     }
 
     fn set_visible(&self) {
