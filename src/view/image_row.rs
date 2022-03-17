f3ed901020237c642660e9c21dcf43ca257d1167
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-03-17 14:06:23 +0100

    image-row: Show image details in its own page
    The approach to use an `AdwExpanderRow` was discarded. Instead, the im-
    age details are now displayed in a separate page as a child of an
    `AdwLeaflet`.

--- src/view/image_row.rs
+++ src/view/image_row.rs
@@ -1,4 +1,4 @@
-use adw::subclass::prelude::{ExpanderRowImpl, PreferencesRowImpl};
+use adw::subclass::prelude::{ActionRowImpl, PreferencesRowImpl};
 use gettextrs::gettext;
 use gtk::glib::{clone, closure, WeakRef};
 use gtk::prelude::*;
@@ -16,32 +16,21 @@ mod imp {
     #[template(resource = "/com/github/marhkb/Symphony/ui/image-row.ui")]
     pub(crate) struct ImageRow {
         pub(super) image: WeakRef<model::Image>,
-        #[template_child]
-        pub(super) id_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) created_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) size_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) containers_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) command_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) entrypoint_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) ports_row: TemplateChild<view::PropertyRow>,
-        #[template_child]
-        pub(super) repo_tags_row: TemplateChild<view::PropertyRow>,
     }
 
     #[glib::object_subclass]
     impl ObjectSubclass for ImageRow {
         const NAME: &'static str = "ImageRow";
         type Type = super::ImageRow;
-        type ParentType = adw::ExpanderRow;
+        type ParentType = adw::ActionRow;
 
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
+
+            klass.install_action("image.show-details", None, move |widget, _, _| {
+                widget.show_details();
+            });
+
             klass.install_action("image.delete", None, move |widget, _, _| {
                 widget.delete();
             });
@@ -92,7 +81,6 @@ mod imp {
             self.parent_constructed(obj);
 
             let image_expr = Self::Type::this_expression("image");
-            let image_config_expr = image_expr.chain_property::<model::Image>("config");
             let repo_tags_expr = image_expr.chain_property::<model::Image>("repo-tags");
 
             repo_tags_expr
@@ -135,135 +123,13 @@ mod imp {
                     id.chars().take(12).collect::<String>()
                 }))
                 .bind(obj, "subtitle", Some(obj));
-
-            image_expr
-                .chain_property::<model::Image>("id")
-                .chain_closure::<String>(closure!(|_: glib::Object, id: &str| {
-                    id.chars().take(12).collect::<String>()
-                }))
-                .bind(&*self.id_row, "value", Some(obj));
-
-            image_expr
-                .chain_property::<model::Image>("created")
-                .chain_closure::<String>(closure!(|_: glib::Object, created: i64| {
-                    glib::DateTime::from_unix_local(created)
-                        .unwrap()
-                        .format("%x %X")
-                        .unwrap()
-                }))
-                .bind(&*self.created_row, "value", Some(obj));
-
-            gtk::ClosureExpression::new::<String, _, _>(
-                &[
-                    image_expr.chain_property::<model::Image>("size").upcast(),
-                    image_expr
-                        .chain_property::<model::Image>("shared-size")
-                        .upcast(),
-                    image_expr
-                        .chain_property::<model::Image>("virtual-size")
-                        .upcast(),
-                ],
-                closure!(
-                    |_: glib::Object, size: u64, shared_size: u64, virtual_size: u64| {
-                        let formatted_size = glib::format_size(size);
-                        if size == shared_size {
-                            if shared_size == virtual_size {
-                                formatted_size.to_string()
-                            } else {
-                                gettext!(
-                                    // Translators: "{}" are placeholders for storage space.
-                                    "{} (Virtual: {})",
-                                    formatted_size,
-                                    glib::format_size(virtual_size),
-                                )
-                            }
-                        } else if size == virtual_size {
-                            if shared_size > 0 {
-                                gettext!(
-                                    // Translators: "{}" are placeholders for storage space.
-                                    "{} (Shared: {})",
-                                    formatted_size,
-                                    glib::format_size(shared_size),
-                                )
-                            } else {
-                                formatted_size.to_string()
-                            }
-                        } else {
-                            gettext!(
-                                // Translators: "{}" are placeholders for storage space.
-                                "{} (Shared: {}, Virtual: {})",
-                                formatted_size,
-                                glib::format_size(shared_size),
-                                glib::format_size(virtual_size),
-                            )
-                        }
-                    }
-                ),
-            )
-            .bind(&*self.size_row, "value", Some(obj));
-
-            image_expr
-                .chain_property::<model::Image>("containers")
-                .chain_closure::<String>(closure!(|_: glib::Object, containers: u64| {
-                    // Translators: "{}" is placeholder for an integer value.
-                    gettext!("By {} containers", containers)
-                }))
-                .bind(&*self.containers_row, "value", Some(obj));
-
-            image_config_expr
-                .chain_property::<model::ImageConfig>("cmd")
-                .chain_closure::<bool>(closure!(|_: glib::Object, cmd: Option<&str>| {
-                    cmd.is_some()
-                }))
-                .bind(&*self.command_row, "visible", Some(obj));
-
-            image_config_expr
-                .chain_property::<model::ImageConfig>("entrypoint")
-                .chain_closure::<bool>(closure!(|_: glib::Object, entrypoint: Option<&str>| {
-                    entrypoint.is_some()
-                }))
-                .bind(&*self.entrypoint_row, "visible", Some(obj));
-
-            image_config_expr
-                .chain_property::<model::ImageConfig>("exposed-ports")
-                .chain_closure::<String>(closure!(
-                    |_: glib::Object, exposed_ports: utils::BoxedStringBTreeSet| {
-                        utils::format_iter(exposed_ports.0.iter(), "\n")
-                    }
-                ))
-                .bind(&*self.ports_row, "value", Some(obj));
-
-            image_config_expr
-                .chain_property::<model::ImageConfig>("exposed-ports")
-                .chain_closure::<bool>(closure!(
-                    |_: glib::Object, exposed_ports: utils::BoxedStringBTreeSet| {
-                        exposed_ports.len() > 0
-                    }
-                ))
-                .bind(&*self.ports_row, "visible", Some(obj));
-
-            image_expr
-                .chain_property::<model::Image>("repo-tags")
-                .chain_closure::<String>(closure!(
-                    |_: glib::Object, repo_tags: utils::BoxedStringVec| {
-                        utils::format_iter(&mut repo_tags.iter(), "; ")
-                    }
-                ))
-                .bind(&*self.repo_tags_row, "value", Some(obj));
-
-            image_expr
-                .chain_property::<model::Image>("repo-tags")
-                .chain_closure::<bool>(closure!(
-                    |_: glib::Object, repo_tags: utils::BoxedStringVec| { repo_tags.len() > 0 }
-                ))
-                .bind(&*self.repo_tags_row, "visible", Some(obj));
         }
     }
 
     impl WidgetImpl for ImageRow {}
     impl ListBoxRowImpl for ImageRow {}
     impl PreferencesRowImpl for ImageRow {}
-    impl ExpanderRowImpl for ImageRow {}
+    impl ActionRowImpl for ImageRow {}
 }
 
 glib::wrapper! {
@@ -313,5 +179,13 @@ impl ImageRow {
                     .priority(adw::ToastPriority::High)
                     .build(),
             );
+    }
+
+    fn show_details(&self) {
+        self.root()
+            .unwrap()
+            .downcast::<Window>()
+            .unwrap()
+            .show_details(&view::ImageDetailsPage::from(&self.image().unwrap()));
     }
 }
