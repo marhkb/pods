c648215ee1d7eb08d8459d4078ac627105b54094
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-05-09 00:03:51 +0200

    image-details-page: Use parent leaflet overlay for hiding details

--- src/view/image/image_details_page.rs
+++ src/view/image/image_details_page.rs
@@ -283,7 +283,7 @@ impl ImageDetailsPage {
     }
 
     fn previous_leaflet_overlay(&self) -> view::LeafletOverlay {
-        utils::find_leaflet_overlay(self)
+        utils::find_parent_leaflet_overlay(self)
     }
 
     fn root_leaflet_overlay(&self) -> view::LeafletOverlay {
