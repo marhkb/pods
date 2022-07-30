ec6d811e4b24d34bb050523fd03328d6d04d4cb9
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-07-30 20:59:26 +0200

    feat(model): Introduce `Pod` and `PodList`

---
+++ src/model/pod.rs
@@ -0,0 +1,357 @@
+use std::cell::Cell;
+use std::cell::RefCell;
+use std::fmt;
+use std::ops::Deref;
+use std::str::FromStr;
+
+use gettextrs::gettext;
+use gtk::glib::clone;
+use gtk::glib::subclass::Signal;
+use gtk::glib::WeakRef;
+use gtk::glib::{self};
+use gtk::prelude::ObjectExt;
+use gtk::prelude::StaticType;
+use gtk::prelude::ToValue;
+use gtk::subclass::prelude::*;
+use once_cell::sync::Lazy;
+use once_cell::unsync::OnceCell;
+
+use crate::api;
+use crate::model;
+use crate::utils;
+
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, glib::Enum)]
+#[enum_type(name = "PodStatus")]
+pub(crate) enum Status {
+    Configured,
+    Created,
+    Dead,
+    Degraded,
+    Exited,
+    Paused,
+    Removing,
+    Restarting,
+    Running,
+    Stopped,
+    Stopping,
+    #[default]
+    Unknown,
+}
+
+impl FromStr for Status {
+    type Err = Self;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "Configured" => Self::Configured,
+            "Created" => Self::Created,
+            "Dead" => Self::Dead,
+            "Degraded" => Self::Degraded,
+            "Exited" => Self::Exited,
+            "Paused" => Self::Paused,
+            "Removing" => Self::Removing,
+            "Restarting" => Self::Restarting,
+            "Running" => Self::Running,
+            "Stopped" => Self::Stopped,
+            "Stopping" => Self::Stopping,
+            _ => return Err(Self::Unknown),
+        })
+    }
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Configured => gettext("Configured"),
+                Self::Created => gettext("Created"),
+                Self::Dead => gettext("Dead"),
+                Self::Degraded => gettext("Degraded"),
+                Self::Exited => gettext("Exited"),
+                Self::Paused => gettext("Paused"),
+                Self::Removing => gettext("Removing"),
+                Self::Restarting => gettext("Restarting"),
+                Self::Running => gettext("Running"),
+                Self::Stopped => gettext("Stopped"),
+                Self::Stopping => gettext("Stopping"),
+                Self::Unknown => gettext("Unknown"),
+            }
+        )
+    }
+}
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    pub(crate) struct Pod {
+        pub(super) pod_list: WeakRef<model::PodList>,
+
+        pub(super) container_list: OnceCell<model::SimpleContainerList>,
+
+        pub(super) created: OnceCell<i64>,
+        pub(super) id: OnceCell<String>,
+        pub(super) name: RefCell<String>,
+        pub(super) num_containers: Cell<i64>,
+        pub(super) status: Cell<Status>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for Pod {
+        const NAME: &'static str = "Pod";
+        type Type = super::Pod;
+    }
+
+    impl ObjectImpl for Pod {
+        fn signals() -> &'static [Signal] {
+            static SIGNALS: Lazy<Vec<Signal>> = Lazy::new(|| {
+                vec![Signal::builder("deleted", &[], <()>::static_type().into()).build()]
+            });
+            SIGNALS.as_ref()
+        }
+
+        fn properties() -> &'static [glib::ParamSpec] {
+            static PROPERTIES: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
+                vec![
+                    glib::ParamSpecObject::new(
+                        "pod-list",
+                        "Pod List",
+                        "The parent pod list",
+                        model::PodList::static_type(),
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                    ),
+                    glib::ParamSpecObject::new(
+                        "container-list",
+                        "Container List",
+                        "The list of containers associated with this Image",
+                        model::SimpleContainerList::static_type(),
+                        glib::ParamFlags::READABLE,
+                    ),
+                    glib::ParamSpecBoolean::new(
+                        "action-ongoing",
+                        "Action Ongoing",
+                        "Whether an action (starting, stopping, etc.) is currently ongoing",
+                        false,
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::EXPLICIT_NOTIFY,
+                    ),
+                    glib::ParamSpecBoolean::new(
+                        "deleted",
+                        "Deleted",
+                        "Whether this pod is deleted",
+                        false,
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::EXPLICIT_NOTIFY,
+                    ),
+                    glib::ParamSpecInt64::new(
+                        "created",
+                        "Created",
+                        "The time when this pod was created",
+                        i64::MIN,
+                        i64::MAX,
+                        0,
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                    ),
+                    glib::ParamSpecString::new(
+                        "id",
+                        "Id",
+                        "The id of this pod",
+                        Option::default(),
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                    ),
+                    glib::ParamSpecString::new(
+                        "name",
+                        "Name",
+                        "The name of this pod",
+                        Option::default(),
+                        glib::ParamFlags::READWRITE
+                            | glib::ParamFlags::CONSTRUCT
+                            | glib::ParamFlags::EXPLICIT_NOTIFY,
+                    ),
+                    glib::ParamSpecInt64::new(
+                        "num-containers",
+                        "Num Containers",
+                        "The number of containers in this pod",
+                        0,
+                        i64::MAX,
+                        0,
+                        glib::ParamFlags::READWRITE
+                            | glib::ParamFlags::CONSTRUCT
+                            | glib::ParamFlags::EXPLICIT_NOTIFY,
+                    ),
+                    glib::ParamSpecEnum::new(
+                        "status",
+                        "Status",
+                        "The status of this pod",
+                        Status::static_type(),
+                        Status::default() as i32,
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::EXPLICIT_NOTIFY,
+                    ),
+                ]
+            });
+            PROPERTIES.as_ref()
+        }
+
+        fn set_property(
+            &self,
+            obj: &Self::Type,
+            _id: usize,
+            value: &glib::Value,
+            pspec: &glib::ParamSpec,
+        ) {
+            match pspec.name() {
+                "pod-list" => self.pod_list.set(value.get().unwrap()),
+                "created" => self.created.set(value.get().unwrap()).unwrap(),
+                "id" => self.id.set(value.get().unwrap()).unwrap(),
+                "name" => obj.set_name(value.get().unwrap()),
+                "num-containers" => obj.set_num_containers(value.get().unwrap()),
+                "status" => obj.set_status(value.get().unwrap()),
+                _ => unimplemented!(),
+            }
+        }
+
+        fn property(&self, obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
+            match pspec.name() {
+                "pod-list" => obj.pod_list().to_value(),
+                "container-list" => obj.container_list().to_value(),
+                "created" => obj.created().to_value(),
+                "id" => obj.id().to_value(),
+                "name" => obj.name().to_value(),
+                "num-containers" => obj.num_containers().to_value(),
+                "status" => obj.status().to_value(),
+                _ => unimplemented!(),
+            }
+        }
+
+        fn constructed(&self, obj: &Self::Type) {
+            self.parent_constructed(obj);
+        }
+    }
+}
+
+glib::wrapper! {
+    pub(crate) struct Pod(ObjectSubclass<imp::Pod>);
+}
+
+impl Pod {
+    pub(crate) fn new(
+        pod_list: &model::PodList,
+        inspect_response: api::LibpodPodInspectResponse,
+    ) -> Self {
+        glib::Object::new(&[
+            ("pod-list", pod_list),
+            (
+                "created",
+                &inspect_response
+                    .created
+                    .map(|dt| dt.timestamp())
+                    .unwrap_or(0),
+            ),
+            ("id", &inspect_response.id),
+            ("name", &inspect_response.name),
+            (
+                "num-containers",
+                &inspect_response.num_containers.unwrap_or(0),
+            ),
+            ("status", &status(inspect_response.state.as_deref())),
+        ])
+        .expect("Failed to create Pod")
+    }
+
+    pub(crate) fn update(&self, inspect_response: api::LibpodPodInspectResponse) {
+        self.set_name(inspect_response.name.unwrap_or_default());
+        self.set_num_containers(inspect_response.num_containers.unwrap_or(0));
+        self.set_status(status(inspect_response.state.as_deref()));
+    }
+
+    pub(crate) fn inspect_and_update(&self) {
+        utils::do_async(
+            {
+                let pod = self.api_pod().unwrap();
+                async move { pod.inspect().await }
+            },
+            clone!(@weak self as obj => move |result| match result {
+                Ok(inspect_response) => obj.update(inspect_response),
+                Err(e) => log::error!("Error on inspecting pod '{}': {e}", obj.id()),
+            }),
+        );
+    }
+
+    pub(crate) fn pod_list(&self) -> Option<model::PodList> {
+        self.imp().pod_list.upgrade()
+    }
+
+    pub(crate) fn container_list(&self) -> &model::SimpleContainerList {
+        self.imp().container_list.get_or_init(Default::default)
+    }
+    pub(crate) fn created(&self) -> i64 {
+        *self.imp().created.get().unwrap()
+    }
+
+    pub(crate) fn id(&self) -> &str {
+        self.imp().id.get().unwrap()
+    }
+
+    pub(crate) fn name(&self) -> String {
+        self.imp().name.borrow().clone()
+    }
+
+    pub(crate) fn set_name(&self, value: String) {
+        if self.name() == value {
+            return;
+        }
+        self.imp().name.replace(value);
+        self.notify("name");
+    }
+
+    pub(crate) fn num_containers(&self) -> i64 {
+        self.imp().num_containers.get()
+    }
+
+    fn set_num_containers(&self, value: i64) {
+        if self.num_containers() == value {
+            return;
+        }
+        self.imp().num_containers.replace(value);
+        self.notify("num-containers");
+    }
+
+    pub(crate) fn status(&self) -> Status {
+        self.imp().status.get()
+    }
+
+    pub(crate) fn set_status(&self, value: Status) {
+        if self.status() == value {
+            return;
+        }
+        self.imp().status.set(value);
+        self.notify("status");
+    }
+
+    pub(crate) fn connect_deleted<F: Fn(&Self) + 'static>(&self, f: F) -> glib::SignalHandlerId {
+        self.connect_local("deleted", true, move |values| {
+            f(&values[0].get::<Self>().unwrap());
+
+            None
+        })
+    }
+
+    pub(crate) fn api_pod(&self) -> Option<api::Pod> {
+        self.pod_list()
+            .unwrap()
+            .client()
+            .map(|client| api::Pod::new(client.podman().deref().clone(), self.id()))
+    }
+}
+
+impl Pod {}
+
+fn status(state: Option<&str>) -> Status {
+    state.map_or_else(Status::default, |s| match Status::from_str(s) {
+        Ok(status) => status,
+        Err(status) => {
+            log::warn!("Unknown container status: {s}");
+            status
+        }
+    })
+}
---
+++ src/model/pod_list.rs
@@ -0,0 +1,368 @@
+use std::cell::Cell;
+use std::cell::RefCell;
+use std::collections::HashSet;
+
+use gtk::gio;
+use gtk::glib;
+use gtk::glib::clone;
+use gtk::glib::subclass::Signal;
+use gtk::glib::WeakRef;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use indexmap::map::Entry;
+use indexmap::map::IndexMap;
+use once_cell::sync::Lazy;
+
+use crate::api;
+use crate::model;
+use crate::utils;
+
+mod imp {
+
+    use super::*;
+
+    #[derive(Debug, Default)]
+    pub(crate) struct PodList {
+        pub(super) client: WeakRef<model::Client>,
+
+        pub(super) fetched: Cell<u32>,
+        pub(super) list: RefCell<IndexMap<String, model::Pod>>,
+        pub(super) listing: Cell<bool>,
+        pub(super) to_fetch: Cell<u32>,
+
+        pub(super) failed: RefCell<HashSet<String>>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for PodList {
+        const NAME: &'static str = "PodList";
+        type Type = super::PodList;
+        type Interfaces = (gio::ListModel,);
+    }
+
+    impl ObjectImpl for PodList {
+        fn signals() -> &'static [Signal] {
+            static SIGNALS: Lazy<Vec<Signal>> = Lazy::new(|| {
+                vec![Signal::builder(
+                    "pod-added",
+                    &[model::Pod::static_type().into()],
+                    <()>::static_type().into(),
+                )
+                .build()]
+            });
+            SIGNALS.as_ref()
+        }
+
+        fn properties() -> &'static [glib::ParamSpec] {
+            static PROPERTIES: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
+                vec![
+                    glib::ParamSpecObject::new(
+                        "client",
+                        "Client",
+                        "The podman client",
+                        model::Client::static_type(),
+                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                    ),
+                    glib::ParamSpecUInt::new(
+                        "fetched",
+                        "Fetched",
+                        "The number of pods that have been fetched",
+                        0,
+                        std::u32::MAX,
+                        0,
+                        glib::ParamFlags::READABLE,
+                    ),
+                    glib::ParamSpecUInt::new(
+                        "len",
+                        "Len",
+                        "The length of this list",
+                        0,
+                        std::u32::MAX,
+                        0,
+                        glib::ParamFlags::READABLE,
+                    ),
+                    glib::ParamSpecBoolean::new(
+                        "listing",
+                        "Listing",
+                        "Wether pods are currently listed",
+                        false,
+                        glib::ParamFlags::READABLE,
+                    ),
+                    glib::ParamSpecUInt::new(
+                        "running",
+                        "Running",
+                        "The number of running pods",
+                        0,
+                        std::u32::MAX,
+                        0,
+                        glib::ParamFlags::READABLE,
+                    ),
+                    glib::ParamSpecUInt::new(
+                        "to-fetch",
+                        "To Fetch",
+                        "The number of pods to be fetched",
+                        0,
+                        std::u32::MAX,
+                        0,
+                        glib::ParamFlags::READABLE,
+                    ),
+                ]
+            });
+            PROPERTIES.as_ref()
+        }
+
+        fn set_property(
+            &self,
+            _obj: &Self::Type,
+            _id: usize,
+            value: &glib::Value,
+            pspec: &glib::ParamSpec,
+        ) {
+            match pspec.name() {
+                "client" => self.client.set(value.get().unwrap()),
+                _ => unimplemented!(),
+            }
+        }
+
+        fn property(&self, obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
+            match pspec.name() {
+                "client" => obj.client().to_value(),
+                "fetched" => obj.fetched().to_value(),
+                "len" => obj.len().to_value(),
+                "listing" => obj.listing().to_value(),
+                "running" => obj.running().to_value(),
+                "to-fetch" => obj.to_fetch().to_value(),
+                _ => unimplemented!(),
+            }
+        }
+        fn constructed(&self, obj: &Self::Type) {
+            self.parent_constructed(obj);
+            obj.connect_items_changed(|self_, _, _, _| self_.notify("len"));
+        }
+    }
+
+    impl ListModelImpl for PodList {
+        fn item_type(&self, _list_model: &Self::Type) -> glib::Type {
+            model::Pod::static_type()
+        }
+
+        fn n_items(&self, _list_model: &Self::Type) -> u32 {
+            self.list.borrow().len() as u32
+        }
+
+        fn item(&self, _list_model: &Self::Type, position: u32) -> Option<glib::Object> {
+            self.list
+                .borrow()
+                .get_index(position as usize)
+                .map(|(_, obj)| obj.upcast_ref())
+                .cloned()
+        }
+    }
+}
+
+glib::wrapper! {
+    pub(crate) struct PodList(ObjectSubclass<imp::PodList>) @implements gio::ListModel;
+}
+
+impl From<Option<&model::Client>> for PodList {
+    fn from(client: Option<&model::Client>) -> Self {
+        glib::Object::new(&[("client", &client)]).expect("Failed to create PodList")
+    }
+}
+
+impl PodList {
+    pub(crate) fn client(&self) -> Option<model::Client> {
+        self.imp().client.upgrade()
+    }
+
+    pub(crate) fn fetched(&self) -> u32 {
+        self.imp().fetched.get()
+    }
+
+    fn set_fetched(&self, value: u32) {
+        if self.fetched() == value {
+            return;
+        }
+        self.imp().fetched.set(value);
+        self.notify("fetched");
+    }
+
+    pub(crate) fn len(&self) -> u32 {
+        self.n_items()
+    }
+
+    pub(crate) fn listing(&self) -> bool {
+        self.imp().listing.get()
+    }
+
+    fn set_listing(&self, value: bool) {
+        if self.listing() == value {
+            return;
+        }
+        self.imp().listing.set(value);
+        self.notify("listing");
+    }
+
+    pub(crate) fn running(&self) -> u32 {
+        self.imp()
+            .list
+            .borrow()
+            .values()
+            .filter(|pod| pod.status() == model::PodStatus::Running)
+            .count() as u32
+    }
+
+    pub(crate) fn to_fetch(&self) -> u32 {
+        self.imp().to_fetch.get()
+    }
+
+    fn set_to_fetch(&self, value: u32) {
+        if self.to_fetch() == value {
+            return;
+        }
+        self.imp().to_fetch.set(value);
+        self.notify("to-fetch");
+    }
+
+    fn add_or_update_pod<F>(&self, id: String, err_op: F)
+    where
+        F: FnOnce(super::RefreshError) + 'static,
+    {
+        utils::do_async(
+            {
+                let id = id.clone();
+                let podman = self.client().unwrap().podman().clone();
+                async move { podman.pods().get(id).inspect().await }
+            },
+            clone!(@weak self as obj => move |result| {
+                let imp = obj.imp();
+                match result {
+                    Ok(inspect_response) => {
+                        let mut list = imp.list.borrow_mut();
+                        let entry = list.entry(id);
+                        match entry {
+                            Entry::Vacant(entry) => {
+                                let pod = model::Pod::new(&obj, inspect_response);
+                                pod.connect_notify_local(
+                                    Some("status"),
+                                    clone!(@weak obj => move |_, _| {
+                                        obj.notify("running");
+                                    }),
+                                );
+                                entry.insert(pod.clone());
+
+                                drop(list);
+                                obj.pod_added(&pod);
+                                obj.items_changed(obj.len() - 1, 0, 1);
+                            }
+                            Entry::Occupied(entry) => {
+                                let pod = entry.get().clone();
+                                drop(list);
+                                pod.update(inspect_response)
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        log::error!("Error on inspecting pod '{id}': {e}");
+                        if imp.failed.borrow_mut().insert(id.clone()) {
+                            err_op(super::RefreshError::Inspect(id));
+                        }
+                    }
+                }
+                obj.set_fetched(obj.fetched() + 1);
+            }),
+        );
+    }
+
+    pub(crate) fn get_pod(&self, id: &str) -> Option<model::Pod> {
+        self.imp().list.borrow().get(id).cloned()
+    }
+
+    pub(crate) fn remove_pod(&self, id: &str) {
+        self.imp().failed.borrow_mut().remove(id);
+
+        let mut list = self.imp().list.borrow_mut();
+        if let Some((idx, _, _)) = list.shift_remove_full(id) {
+            drop(list);
+            self.items_changed(idx as u32, 1, 0);
+        }
+    }
+
+    pub(crate) fn refresh<F>(&self, err_op: F)
+    where
+        F: FnOnce(super::RefreshError) + Clone + 'static,
+    {
+        self.set_listing(true);
+        utils::do_async(
+            {
+                let podman = self.client().unwrap().podman().clone();
+                async move {
+                    podman
+                        .pods()
+                        .list(&api::PodListOpts::builder().build())
+                        .await
+                }
+            },
+            clone!(@weak self as obj => move |result| {
+                obj.set_listing(false);
+                match result {
+                    Ok(list_pods) => {
+                        let to_remove = obj
+                            .imp()
+                            .list
+                            .borrow()
+                            .keys()
+                            .filter(|id| {
+                                !list_pods
+                                    .iter()
+                                    .any(|summary| summary.id.as_ref() == Some(id))
+                            })
+                            .cloned()
+                            .collect::<Vec<_>>();
+                        to_remove.iter().for_each(|id| obj.remove_pod(id));
+
+                        obj.set_fetched(0);
+                        obj.set_to_fetch(list_pods.len() as u32);
+
+                        list_pods.into_iter().for_each(|list_container| {
+                            obj.add_or_update_pod(list_container.id.unwrap(), err_op.clone());
+                        });
+                    }
+                    Err(e) => {
+                        log::error!("Error on retrieving pods: {}", e);
+                        err_op(super::RefreshError::List);
+                    }
+                }
+            }),
+        );
+    }
+
+    pub(crate) fn handle_event<F>(&self, event: api::Event, err_op: F)
+    where
+        F: FnOnce(super::RefreshError) + Clone + 'static,
+    {
+        let pod_id = event.actor.id;
+
+        match event.action.as_str() {
+            "remove" => self.remove_pod(&pod_id),
+            _ => self.add_or_update_pod(pod_id, err_op),
+        }
+    }
+
+    fn pod_added(&self, pod: &model::Pod) {
+        self.emit_by_name::<()>("pod-added", &[pod]);
+    }
+
+    pub(crate) fn connect_pod_added<F: Fn(&Self, &model::Pod) + 'static>(
+        &self,
+        f: F,
+    ) -> glib::SignalHandlerId {
+        self.connect_local("pod-added", true, move |values| {
+            let obj = values[0].get::<Self>().unwrap();
+            let pod = values[1].get::<model::Pod>().unwrap();
+            f(&obj, &pod);
+
+            None
+        })
+    }
+}
