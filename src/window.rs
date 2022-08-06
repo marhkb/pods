1da9eb9962fc13b870a806aa3570cc61bf36f529
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-08-06 22:54:44 +0200

    refactor: Move pod action into panel
    We put the action into a button, which we attach to the preferences
    group.

--- src/window.rs
+++ src/window.rs
@@ -122,16 +122,6 @@ mod imp {
             });
 
             klass.add_binding_action(
-                gdk::Key::P,
-                gdk::ModifierType::CONTROL_MASK,
-                "pod.create",
-                None,
-            );
-            klass.install_action("pod.create", None, move |widget, _, _| {
-                widget.create_pod();
-            });
-
-            klass.add_binding_action(
                 gdk::Key::F,
                 gdk::ModifierType::CONTROL_MASK,
                 "win.toggle-search",
@@ -394,16 +384,6 @@ impl Window {
             ..set_transient_for(Some(self));
         }
         .present();
-    }
-
-    fn create_pod(&self) {
-        let leaflet_overlay = &*self.imp().leaflet_overlay;
-
-        if leaflet_overlay.child().is_none() {
-            leaflet_overlay.show_details(&view::PodCreationPage::from(
-                self.connection_manager().client().as_ref(),
-            ));
-        }
     }
 
     fn toggle_search(&self) {
