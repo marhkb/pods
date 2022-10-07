9faca0f7b647a21256052f09e719e7e5837c339e
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-10-07 20:49:29 +0200

    feat(action-menu-button): Show a simple progress bar

--- src/view/actions/menu_button.rs
+++ src/view/actions/menu_button.rs
@@ -12,6 +12,7 @@ use crate::utils;
 use crate::view;
 
 const ACTION_CLEAN_UP: &str = "actions-menu-button.clean-up";
+const PROGRESS_BAR_WIDTH: f64 = 24.0;
 
 mod imp {
     use super::*;
@@ -21,6 +22,10 @@ mod imp {
     pub(crate) struct MenuButton {
         pub(super) action_list: WeakRef<model::ActionList>,
         pub(super) overview: view::ActionsOverview,
+        #[template_child]
+        pub(super) progress_bar: TemplateChild<gtk::Fixed>,
+        #[template_child]
+        pub(super) progress_bar_through: TemplateChild<adw::Bin>,
         #[template_child]
         pub(super) menu_button: TemplateChild<gtk::MenuButton>,
         #[template_child]
@@ -95,6 +100,7 @@ mod imp {
 
             let action_list_expr = Self::Type::this_expression("action-list");
             let len_expr = action_list_expr.chain_property::<model::ActionList>("len");
+            let ongoing_expr = action_list_expr.chain_property::<model::ActionList>("ongoing");
 
             action_list_expr.bind(&self.overview, "action-list", Some(obj));
 
@@ -105,19 +111,25 @@ mod imp {
                     action_list_expr.chain_property::<model::ActionList>("ongoing"),
                 ],
                 closure!(|_: Self::Type, failed: u32, cancelled: u32, ongoing: u32| {
-                        vec![if failed > 0 {
-                            "failed"
-                        } else if cancelled > 0 {
-                            "cancelled"
+                    vec![if failed > 0 {
+                        "failed"
+                    } else if cancelled > 0 {
+                        "cancelled"
                     } else if ongoing > 0 {
                         "good"
                     } else {
-                            "finished"
-                        }
-                        .to_string()]
+                        "finished"
+                    }
+                    .to_string()]
                 }),
             )
             .bind(obj, "css-classes", Some(obj));
+
+            ongoing_expr
+                .chain_closure::<i32>(closure!(|_: Self::Type, ongoing: u32| {
+                    PROGRESS_BAR_WIDTH as i32 / (ongoing + 1) as i32
+                }))
+                .bind(&*self.progress_bar_through, "width-request", Some(obj));
 
             len_expr.watch(
                 Some(obj),
@@ -128,6 +140,10 @@ mod imp {
                     );
                 }),
             );
+
+            len_expr
+                .chain_closure::<bool>(closure!(|_: Self::Type, len: u32| len > 0))
+                .bind(&*self.progress_bar, "visible", Some(obj));
         }
 
         fn dispose(&self, obj: &Self::Type) {
