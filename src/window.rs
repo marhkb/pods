b0518fc5799bd5dfe34f1a962d48053f9810ab45
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2023-03-07 20:19:03 +0100

    feat(app-window): Make it closable by `Ctrl+W`
    Fixes #585

--- src/window.rs
+++ src/window.rs
@@ -138,6 +138,16 @@ mod imp {
             view::WelcomePage::static_type();
             view::ZoomControl::static_type();
 
+            klass.install_action("win.close", None, |widget, _, _| {
+                widget.close();
+            });
+            klass.add_binding_action(
+                gdk::Key::W,
+                gdk::ModifierType::CONTROL_MASK,
+                "win.close",
+                None,
+            );
+
             klass.add_binding_action(
                 gdk::Key::Home,
                 gdk::ModifierType::ALT_MASK,
