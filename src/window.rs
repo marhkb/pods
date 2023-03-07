b149fa0310d327448b0b8122c0e0691d062403ae
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2023-03-07 20:18:30 +0100

    feat(help-overlay): Make it closable by `Ctrl+W`
    Fixes #585

--- src/window.rs
+++ src/window.rs
@@ -416,7 +416,24 @@ mod imp {
         }
     }
 
-    impl WidgetImpl for Window {}
+    impl WidgetImpl for Window {
+        fn realize(&self) {
+            let dialog = self.obj().help_overlay().unwrap();
+
+            let controller = gtk::EventControllerKey::new();
+            controller.connect_key_pressed(clone!(
+                @weak dialog => @default-return glib::signal::Inhibit(true), move |_, key, _, modifier| {
+                    if key == gdk::Key::w && modifier == gdk::ModifierType::CONTROL_MASK {
+                        dialog.close();
+                    }
+                    glib::signal::Inhibit(false)
+                }
+            ));
+            dialog.add_controller(&controller);
+
+            self.parent_realize();
+        }
+    }
 
     impl WindowImpl for Window {
         // Save window state on delete event
