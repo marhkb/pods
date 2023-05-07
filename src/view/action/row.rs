8b937377b2b6ba1d724f53278d6bab0e66a81150
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2023-05-07 21:39:57 +0200

    feat(action-row): Handle pruning of pods

--- src/view/action/row.rs
+++ src/view/action/row.rs
@@ -85,7 +85,7 @@ mod imp {
                     use model::ActionType::*;
 
                     match type_ {
-                        PruneImages => "eraser5-symbolic",
+                        PruneImages | PrunePods => "eraser5-symbolic",
                         DownloadImage => "folder-download-symbolic",
                         BuildImage => "build-configure-symbolic",
                         Commit => "merge-symbolic",
