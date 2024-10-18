bf3cf246fb0e50d7a1d2bc4c191f8454b2eacac9
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2024-10-18 11:40:49 +0200

    fix(pods-panel): Correctly reset search

--- src/view/pods_panel.rs
+++ src/view/pods_panel.rs
@@ -143,7 +143,7 @@ mod imp {
             );
 
             klass.install_action(ACTION_SHOW_ALL_PODS, None, |widget, _, _| {
-                widget.set_show_only_running_pods(false);
+                widget.show_all_pods();
             });
         }
 
@@ -353,27 +353,26 @@ mod imp {
                 view::PodRow::from(item.downcast_ref().unwrap()).upcast()
             });
 
+            self.set_filter_stack_visible_child(value, &model);
             model.connect_items_changed(clone!(
                 #[weak]
                 obj,
+                #[weak]
+                value,
                 move |model, _, removed, _| {
-                    obj.imp().filter_stack.set_visible_child_name(
-                        if model.n_items() > 0
-                            || !obj
-                                .pod_list()
-                                .as_ref()
-                                .is_some_and(model::PodList::initialized)
-                        {
-                            "list"
-                        } else {
-                            "empty"
-                        },
-                    );
+                    obj.imp().set_filter_stack_visible_child(&value, model);
 
                     if removed > 0 {
                         obj.deselect_hidden_pods(model.upcast_ref());
                     }
                 }
+            ));
+            value.connect_initialized_notify(clone!(
+                #[weak]
+                obj,
+                #[weak]
+                model,
+                move |value| obj.imp().set_filter_stack_visible_child(value, &model)
             ));
 
             ACTIONS_SELECTION
@@ -391,6 +390,20 @@ mod imp {
             );
 
             self.pod_list.set(Some(value));
+        }
+
+        fn set_filter_stack_visible_child(
+            &self,
+            pod_list: &model::PodList,
+            model: &impl IsA<gio::ListModel>,
+        ) {
+            self.filter_stack.set_visible_child_name(
+                if model.n_items() > 0 || !pod_list.initialized() {
+                    "list"
+                } else {
+                    "empty"
+                },
+            );
         }
 
         fn update_filter(&self, filter_change: gtk::FilterChange) {
@@ -414,6 +427,11 @@ impl Default for PodsPanel {
 }
 
 impl PodsPanel {
+    pub(crate) fn show_all_pods(&self) {
+        self.set_show_only_running_pods(false);
+        self.set_search_mode(false);
+    }
+
     pub(crate) fn set_search_mode(&self, value: bool) {
         self.imp().search_bar.set_search_mode(value);
     }
