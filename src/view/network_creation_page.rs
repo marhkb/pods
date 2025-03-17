b2c3ecb9835eac02107955b931af4672707bc12a
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2025-03-17 19:18:56 +0100

    wip

--- src/view/network_creation_page.rs
+++ src/view/network_creation_page.rs
@@ -17,13 +17,7 @@ use crate::utils;
 use crate::view;
 use crate::widget;
 
-const ACTION_CREATE: &str = "pod-creation-page.create";
-const ACTION_ADD_LABEL: &str = "pod-creation-page.add-label";
-const ACTION_ADD_HOST: &str = "pod-creation-page.add-host";
-const ACTION_ADD_PORT_MAPPING: &str = "pod-creation-page.add-port-mapping";
-const ACTION_ADD_DEVICE: &str = "pod-creation-page.add-device";
-const ACTION_ADD_INFRA_CMD_ARGS: &str = "pod-creation-page.add-infra-cmd-arg";
-const ACTION_ADD_POD_CREATE_CMD_ARGS: &str = "pod-creation-page.add-pod-create-cmd-arg";
+const ACTION_CREATE: &str = "network-creation-page.create";
 
 mod imp {
     use super::*;
@@ -32,14 +26,6 @@ mod imp {
     #[properties(wrapper_type = super::NetworkCreationPage)]
     #[template(resource = "/com/github/marhkb/Pods/ui/view/network_creation_page.ui")]
     pub(crate) struct NetworkCreationPage {
-        pub(super) labels: OnceCell<gio::ListStore>,
-        pub(super) hosts: OnceCell<gio::ListStore>,
-        pub(super) port_mappings: OnceCell<gio::ListStore>,
-        pub(super) devices: OnceCell<gio::ListStore>,
-        pub(super) pod_create_cmd_args: OnceCell<gio::ListStore>,
-        pub(super) infra_cmd_args: OnceCell<gio::ListStore>,
-        pub(super) command_row_handler:
-            RefCell<Option<(glib::SignalHandlerId, glib::WeakRef<model::Image>)>>,
         #[property(get, set, construct_only, nullable)]
         pub(super) client: glib::WeakRef<model::Client>,
         #[property(get, set, construct_only, nullable)]
@@ -52,40 +38,6 @@ mod imp {
         pub(super) create_button: TemplateChild<gtk::Button>,
         #[template_child]
         pub(super) name_entry_row: TemplateChild<widget::RandomNameEntryRow>,
-        #[template_child]
-        pub(super) hostname_entry_row: TemplateChild<adw::EntryRow>,
-        #[template_child]
-        pub(super) pod_create_command_entry_row: TemplateChild<adw::EntryRow>,
-        #[template_child]
-        pub(super) pod_create_command_arg_list_box: TemplateChild<gtk::ListBox>,
-        #[template_child]
-        pub(super) hosts_list_box: TemplateChild<gtk::ListBox>,
-        #[template_child]
-        pub(super) labels_list_box: TemplateChild<gtk::ListBox>,
-        #[template_child]
-        pub(super) port_mapping_list_box: TemplateChild<gtk::ListBox>,
-        #[template_child]
-        pub(super) devices_list_box: TemplateChild<gtk::ListBox>,
-        #[template_child]
-        pub(super) enable_hosts_switch: TemplateChild<gtk::Switch>,
-        #[template_child]
-        pub(super) disable_resolv_switch_row: TemplateChild<adw::SwitchRow>,
-        #[template_child]
-        pub(super) disable_infra_switch_row: TemplateChild<adw::SwitchRow>,
-        #[template_child]
-        pub(super) infra_settings_box: TemplateChild<gtk::Box>,
-        #[template_child]
-        pub(super) infra_name_entry_row: TemplateChild<adw::EntryRow>,
-        #[template_child]
-        pub(super) infra_pull_latest_image_switch_row: TemplateChild<adw::SwitchRow>,
-        #[template_child]
-        pub(super) infra_image_selection_combo_row: TemplateChild<view::ImageSelectionComboRow>,
-        #[template_child]
-        pub(super) infra_common_pid_file_entry_row: TemplateChild<adw::EntryRow>,
-        #[template_child]
-        pub(super) infra_command_entry_row: TemplateChild<adw::EntryRow>,
-        #[template_child]
-        pub(super) infra_command_arg_list_box: TemplateChild<gtk::ListBox>,
     }
 
     #[glib::object_subclass]
@@ -100,24 +52,6 @@ mod imp {
 
             klass.install_action(ACTION_CREATE, None, |widget, _, _| {
                 widget.finish();
-            });
-            klass.install_action(ACTION_ADD_LABEL, None, |widget, _, _| {
-                widget.add_label();
-            });
-            klass.install_action(ACTION_ADD_HOST, None, |widget, _, _| {
-                widget.add_host();
-            });
-            klass.install_action(ACTION_ADD_PORT_MAPPING, None, |widget, _, _| {
-                widget.add_port_mapping();
-            });
-            klass.install_action(ACTION_ADD_DEVICE, None, |widget, _, _| {
-                widget.add_device();
-            });
-            klass.install_action(ACTION_ADD_POD_CREATE_CMD_ARGS, None, |widget, _, _| {
-                widget.add_pod_create_cmd_arg();
-            });
-            klass.install_action(ACTION_ADD_INFRA_CMD_ARGS, None, |widget, _, _| {
-                widget.add_infra_cmd_arg();
             });
         }
 
@@ -141,79 +75,6 @@ mod imp {
 
         fn constructed(&self) {
             self.parent_constructed();
-
-            let obj = &*self.obj();
-
-            bind_model(
-                &self.labels_list_box,
-                self.labels(),
-                |item| {
-                    view::KeyValRow::from(item.downcast_ref::<model::KeyVal>().unwrap()).upcast()
-                },
-                ACTION_ADD_LABEL,
-                &gettext("Add Label"),
-            );
-
-            bind_model(
-                &self.hosts_list_box,
-                self.hosts(),
-                |item| {
-                    view::KeyValRow::new(
-                        &gettext("Hostname"),
-                        &gettext("IP"),
-                        item.downcast_ref::<model::KeyVal>().unwrap(),
-                    )
-                    .upcast()
-                },
-                ACTION_ADD_HOST,
-                &gettext("Add Host"),
-            );
-
-            bind_model(
-                &self.port_mapping_list_box,
-                self.port_mappings(),
-                |item| {
-                    view::PortMappingRow::from(item.downcast_ref::<model::PortMapping>().unwrap())
-                        .upcast()
-                },
-                ACTION_ADD_PORT_MAPPING,
-                &gettext("Add Port Mapping"),
-            );
-
-            bind_model(
-                &self.devices_list_box,
-                self.devices(),
-                |item| {
-                    view::DeviceRow::from(item.downcast_ref::<model::Device>().unwrap()).upcast()
-                },
-                ACTION_ADD_DEVICE,
-                &gettext("Add Device"),
-            );
-
-            bind_model(
-                &self.pod_create_command_arg_list_box,
-                self.pod_create_cmd_args(),
-                |item| {
-                    view::ValueRow::new(item.downcast_ref().unwrap(), &gettext("Argument")).upcast()
-                },
-                ACTION_ADD_POD_CREATE_CMD_ARGS,
-                &gettext("Add Argument"),
-            );
-
-            bind_model(
-                &self.infra_command_arg_list_box,
-                self.infra_cmd_args(),
-                |item| {
-                    view::ValueRow::new(item.downcast_ref().unwrap(), &gettext("Argument")).upcast()
-                },
-                ACTION_ADD_INFRA_CMD_ARGS,
-                &gettext("Add Argument"),
-            );
-
-            self.infra_image_selection_combo_row
-                .set_client(obj.client());
-
-            obj.update_infra_command_row();
         }
 
         fn dispose(&self) {
@@ -252,69 +113,6 @@ mod imp {
         fn on_name_entry_row_changed(&self) {
             self.obj().on_name_changed();
         }
-
-        #[template_callback]
-        fn on_disable_resolv_switch_row_active_changed(&self) {
-            if self.disable_resolv_switch_row.is_active() {
-                self.disable_infra_switch_row.set_active(false);
-                self.infra_settings_box.set_visible(true);
-            } else {
-                self.hosts_list_box.set_visible(true);
-            }
-        }
-
-        #[template_callback]
-        fn on_enable_hosts_switch_active_changed(&self) {
-            if self.enable_hosts_switch.is_active() {
-                self.hosts_list_box.set_visible(true);
-            } else {
-                self.hosts_list_box.set_visible(false);
-            }
-        }
-
-        #[template_callback]
-        fn on_disable_infra_switch_row_active_changed(&self) {
-            if self.disable_infra_switch_row.is_active() {
-                self.infra_settings_box.set_visible(false);
-                self.disable_resolv_switch_row.set_active(false);
-            } else {
-                self.infra_settings_box.set_visible(true);
-            }
-        }
-
-        #[template_callback]
-        fn on_infra_image_selection_combo_row_notify_subtitle(&self) {
-            self.obj().update_infra_command_row();
-        }
-
-        pub(super) fn labels(&self) -> &gio::ListStore {
-            self.labels
-                .get_or_init(gio::ListStore::new::<model::KeyVal>)
-        }
-
-        pub(super) fn hosts(&self) -> &gio::ListStore {
-            self.hosts.get_or_init(gio::ListStore::new::<model::KeyVal>)
-        }
-
-        pub(super) fn port_mappings(&self) -> &gio::ListStore {
-            self.port_mappings
-                .get_or_init(gio::ListStore::new::<model::PortMapping>)
-        }
-
-        pub(super) fn devices(&self) -> &gio::ListStore {
-            self.devices
-                .get_or_init(gio::ListStore::new::<model::Device>)
-        }
-
-        pub(super) fn pod_create_cmd_args(&self) -> &gio::ListStore {
-            self.pod_create_cmd_args
-                .get_or_init(gio::ListStore::new::<model::Value>)
-        }
-
-        pub(super) fn infra_cmd_args(&self) -> &gio::ListStore {
-            self.infra_cmd_args
-                .get_or_init(gio::ListStore::new::<model::Value>)
-        }
     }
 }
 
@@ -340,30 +138,6 @@ impl NetworkCreationPage {
 
     fn on_name_changed(&self) {
         self.action_set_enabled(ACTION_CREATE, self.imp().name_entry_row.text().len() > 0);
-    }
-
-    fn add_label(&self) {
-        add_key_val(self.imp().labels());
-    }
-
-    fn add_host(&self) {
-        add_key_val(self.imp().hosts());
-    }
-
-    fn add_port_mapping(&self) {
-        add_port_mapping(self.imp().port_mappings());
-    }
-
-    fn add_device(&self) {
-        add_device(self.imp().devices());
-    }
-
-    fn add_pod_create_cmd_arg(&self) {
-        add_value(self.imp().pod_create_cmd_args());
-    }
-
-    fn add_infra_cmd_arg(&self) {
-        add_value(self.imp().infra_cmd_args());
     }
 
     fn finish(&self) {
@@ -418,40 +192,6 @@ impl NetworkCreationPage {
             // );
             ;
         opts
-    }
-
-    fn update_infra_command_row(&self) {
-        let imp = self.imp();
-
-        match imp.infra_image_selection_combo_row.image() {
-            Some(image) => match image.data() {
-                Some(details) => imp
-                    .infra_command_entry_row
-                    .set_text(&details.config().cmd().unwrap_or_default()),
-                None => {
-                    if let Some((handler, image)) = imp.command_row_handler.take() {
-                        if let Some(image) = image.upgrade() {
-                            image.disconnect(handler);
-                        }
-                    }
-                    let handler = image.connect_data_notify(clone!(
-                        #[weak(rename_to = obj)]
-                        self,
-                        move |image| {
-                            obj.imp().infra_command_entry_row.set_text(
-                                &image.data().unwrap().config().cmd().unwrap_or_default(),
-                            );
-                        }
-                    ));
-                    let image_weak = glib::WeakRef::new();
-                    image_weak.set(Some(&image));
-                    imp.command_row_handler.replace(Some((handler, image_weak)));
-
-                    image.inspect(|_| {});
-                }
-            },
-            None => imp.infra_command_entry_row.set_text(""),
-        }
     }
 }
 
